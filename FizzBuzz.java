aa783966b406c6c7e46f212a4b8871a5326ef8cb
Yahya Faris <54645102+exelr8@users.noreply.github.com>
2023-02-19 19:50:28 +0100

    Add files via upload
    the main program

---
+++ FizzBuzz.java
@@ -0,0 +1,30 @@
+package com.yahya;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+        public static void main (String[] args) {
+          
+          
+          
+          Scanner scanner = new Scanner(System.in);
+          System.out.print("Enter a Number: ");
+          int number = scanner.nextInt();
+
+          if (number % 3 == 0 && number % 5 == 0)
+                System.out.println("Fizz Buzz");
+          else if (number % 3 == 0)
+                System.out.println("Buzz");
+          else if (number % 5  == 0)
+            System.out.println("Fizz");
+          else
+             System.out.println(number);
+
+            
+
+
+
+
+    }
+       
+}
